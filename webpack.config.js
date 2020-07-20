6ee716b46d72f73a5b11d501730b1621c7b06171
Zachary Svoboda <cczac1@gmail.com>
2020-07-19 21:02:25 -0400

    New dist

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'parser.min.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
